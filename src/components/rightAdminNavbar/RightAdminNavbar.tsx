b540e5fa96d577a0c449610af98bcf4c263b7518
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2023-02-02 21:44:28 +0500

    settings

--- src/components/rightAdminNavbar/RightAdminNavbar.tsx
+++ src/components/rightAdminNavbar/RightAdminNavbar.tsx
@@ -1,27 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import api from '../../utility/api'
-import RightAdminNavbarIcons from './RightAdminNavbar_Icons'
+import api from "../../utility/api";
+import RightAdminNavbarIcons from "./RightAdminNavbar_Icons";
 
-import styles from './RightAdminNavbar.module.css'
-import RightAdminNavbarBlock from './RightAdminNavbar_Block'
+import styles from "./RightAdminNavbar.module.css";
+import RightAdminNavbarBlock from "./RightAdminNavbar_Block";
 
 const RightAdminNavbar = () => {
   const [data, setData] = useState({
-    name: 'Undefined',
-    surname: 'Undefined',
+    name: "Undefined",
+    surname: "Undefined",
     image:
-      'https://www.kindpng.com/picc/m/247-2472302_admin-transparent-background-admin-icon-hd-png-download.png',
-  })
+      "https://www.kindpng.com/picc/m/247-2472302_admin-transparent-background-admin-icon-hd-png-download.png",
+  });
 
   useEffect(() => {
-    api.get('/').then((res) => {
-      setData(res.data)      
-    })
-  }, [])
+    api.get("/").then((res) => {
+      if (res.data) {
+        setData(res.data);
+      }
+    });
+  }, []);
 
   if (!data) {
-    return <h1>Wait...</h1>
+    return <h1>Wait...</h1>;
   }
   return (
     <div className={styles.body}>
@@ -31,7 +33,7 @@ const RightAdminNavbar = () => {
         <div className={styles.header__adminInfo__wrap}>
           <div className={styles.header__adminInfo}>
             <h1>
-              {data.name} {data.surname[0]}.
+              {data?.name} {data?.surname[0]}.
             </h1>
             <p>Admin</p>
           </div>
@@ -47,7 +49,7 @@ const RightAdminNavbar = () => {
         <RightAdminNavbarBlock />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RightAdminNavbar
+export default RightAdminNavbar;
--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -1,30 +1,74 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from "react";
+import { BsFillPencilFill, BsFillSaveFill } from "react-icons/bs";
 
-import api from '../../utility/api';
-import styles from './Settings.module.css'
+import api from "../../utility/api";
+import styles from "./Settings.module.css";
 
-type AdminData = {
-  data:{
-    name: string
-    surname: string
-    image: string
-  }
-}
+type DataType = {
+  name: string;
+  surname: string;
+  image: string;
+};
+
+type ResType = {
+  data: DataType;
+};
 
 const Settings = () => {
-  // const [data, setData] = useState([])
+  const [data, setData] = useState<DataType>({
+    name: "string;",
+    surname: "string;",
+    image: "string;",
+  });
+  const [str, setStr] = useState("");
+
+  const [type, setType] = useState(false);
+
   useEffect(() => {
-    api.get('/').then((res:AdminData)=>{
-      console.log(res.data)
-    })
+    api.get("/").then((res: ResType) => {
+      setData(res.data);
+      setStr(res.data.name + " " + res.data.surname);
+    });
   }, []);
+
+  const data3 = { 
+    name: str.split(' ', 1),
+    surname: str.split(' ', 2)[1],
+    image: 'sdfsdfsdf'
+  }
+  const edit = () => {
+    api.put("/settings", data3).then((res) => {
+      setData(res.data);
+    });
+    setType(false);
+  };
+
   return (
     <div className={styles.body}>
-      <div>
-        <img src="" alt="" />
+      <div className={styles.img_back}>
+        <img src={data.image} alt="dsfdsf" />
+      </div>
+      <div className={styles.block}>
+        {!type ? (
+          <>
+            <h1>
+              {data.name} {data.surname}
+            </h1>{" "}
+            <BsFillPencilFill
+              onClick={() => {
+                setType(true);
+              }}
+            />
+          </>
+        ) : (
+          <>
+            <input value={str} onChange={(e) => setStr(e.target.value)} />
+            <BsFillSaveFill onClick={edit} />
+          </>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Settings
+export default Settings;
