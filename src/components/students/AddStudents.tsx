89ecef2ec72a7670f71cd781f11672db596d539c
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2023-02-07 19:41:40 +0500

    change;

--- src/components/students/AddStudents.tsx
+++ src/components/students/AddStudents.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import styles from './AddStudents.module.css'
+import React from "react";
+import styles from "./AddStudents.module.css";
 
 const AddStudents = () => {
   return (
@@ -7,19 +7,25 @@ const AddStudents = () => {
       <div className={styles.student_details}>
         <h1>Student Details</h1>
         <main>
-          <div>
+          <div className={styles.student_details_block}>
             <h2>First Name *</h2>
+            <input placeholder="Samantha" />
           </div>
+          <div className={styles.student_details_block}>
+            <h2>Last Name *</h2>
+            <input placeholder="William" />
+          </div>
+          <div className={styles.student_details_block}>
+          <h2>Date & Place of Birth *</h2>
           <div>
-
+            <input type={'date'} placeholder="02/24/1997" />
+          </div>
           </div>
         </main>
       </div>
-      <div className={styles.parents_details}>
-
-      </div>
+      <div className={styles.parents_details}></div>
     </div>
-  )
-}
+  );
+};
 
-export default AddStudents
+export default AddStudents;
