8b9d6ccbc535ae3141f7756a13acc83208a096fd
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2023-02-07 21:35:35 +0500

    change;

--- src/components/students/AddStudents.tsx
+++ src/components/students/AddStudents.tsx
@@ -16,10 +16,27 @@ const AddStudents = () => {
             <input placeholder="William" />
           </div>
           <div className={styles.student_details_block}>
-          <h2>Date & Place of Birth *</h2>
-          <div>
-            <input type={'date'} placeholder="02/24/1997" />
+            <h2>Date & Place of Birth *</h2>
+            <div>
+              <input type={"date"} placeholder="02/24/1997" />
+              <input type={"type"} placeholder="Jakarta" />
+            </div>
           </div>
+          <div className={styles.student_details_block}>
+            <h2>Parent Name *</h2>
+            <input placeholder="William" />
+          </div>
+          <div className={styles.student_details_block}>
+            <h2>Email *</h2>
+            <input type={"email"} placeholder="william@mail.com" />
+          </div>
+          <div className={styles.student_details_block}>
+            <h2>Phone *</h2>
+            <input type={"tel"} placeholder="+1234567890" />
+          </div>
+          <div className={styles.student_details_block}>
+            <h2>Address *</h2>
+            <textarea maxLength={2000} placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " />
           </div>
         </main>
       </div>
