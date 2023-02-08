956ab1bb1aeb4c4548b0db6f5d0572dce4ef3ad2
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2023-02-08 21:50:50 +0500

    change

--- src/components/students/AddStudents.tsx
+++ src/components/students/AddStudents.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import useWindowSize from './../../utility/hooks';
+
 import styles from "./AddStudents.module.css";
 
 const AddStudents = () => {
+  const { width } = useWindowSize()
+
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
   return (
     <div className={styles.wrap}>
       <div className={styles.student_details}>
@@ -34,9 +39,17 @@ const AddStudents = () => {
             <h2>Phone *</h2>
             <input type={"tel"} placeholder="+1234567890" />
           </div>
-          <div className={styles.student_details_block}>
+          <div className={styles.student_details_block + ' ' + styles.student_details_block_text}>
             <h2>Address *</h2>
-            <textarea maxLength={2000} placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " />
+            <textarea
+              style={{ resize: "none" }}
+              maxLength={2000}
+              placeholder={width < 500 ? text.slice(0,100) + '...' : text}
+            />
+          </div>
+          <div className={styles.student_details_block + ' ' + styles.student_details__btn}>
+            <button>Save as Draft</button>
+            <button>Submit</button>
           </div>
         </main>
       </div>
